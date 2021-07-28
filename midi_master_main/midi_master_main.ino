823ae648f2e71eeabbe2133f540a7e93a1e1d411
Niek ten Brinke <niektb95@gmail.com>
2021-07-28 21:14:23 +0200

    Implement RC500

--- midi_master_main/midi_master_main.ino
+++ midi_master_main/midi_master_main.ino
@@ -6,15 +6,18 @@ Switch switch2 = Switch(3U);
 Switch switch3 = Switch(2U);
 
 // MIDI PARAM
-const uint8_t programChangeLine6M5 = 192;
-//const uint8_t controlChangeLine6M5 = 176;
-const uint8_t favPresetLine6M5 = 7;
-uint8_t presetLine6M5 = favPresetLine6M5;
+const uint8_t controlChangeBossRC500 = 176;
 
 // Send MIDI message
 void sendMidiProgramChange(int COMMAND, int DATA) {
     Serial.write(COMMAND);
     Serial.write(DATA);
+}
+
+void sendMidiControlChange(int COMMAND, int DATA1, int DATA2) {
+    Serial.write(COMMAND);
+    Serial.write(DATA1);
+    Serial.write(DATA2);
 }
 
 /* TASKS */
@@ -52,33 +55,11 @@ void taskMan(xTaskId id_)
     {
         // Perform task if we got a notification
         if (strcmp(res->notifyValue, "PRE1") == 0)
-        {
-            // Press Switch 1
-            if (presetLine6M5 > 0)
-                presetLine6M5--;
-            else
-                presetLine6M5 = 23;  
-            
-            sendMidiProgramChange(programChangeLine6M5, presetLine6M5);
-        }
+            sendMidiControlChange(controlChangeBossRC500, 16, 127);
         else if (strcmp(res->notifyValue, "PRE2") == 0)
-        {
-            // Press Switch 2
-            presetLine6M5 = favPresetLine6M5;
-          
-            sendMidiProgramChange(programChangeLine6M5, presetLine6M5);
-        }
+            sendMidiControlChange(controlChangeBossRC500, 17, 127);
         else if (strcmp(res->notifyValue, "PRE3") == 0)
-        {
-            // Press Switch 3
-            if (presetLine6M5 < 23)
-                presetLine6M5++;
-            else
-                presetLine6M5 = 0;
-            
-            sendMidiProgramChange(programChangeLine6M5, presetLine6M5);
-        }
-    //xTaskNotify(xTaskGetId("TASKSERIAL"), 4, res->notifyValue);
+            sendMidiControlChange(controlChangeBossRC500, 18, 127);
     }
     xMemFree(res);
     xTaskNotifyClear(id_);
@@ -89,8 +70,7 @@ void setup()
     Serial.begin(31250);
     //Serial.println(F("[MIDI Master Debug Stream]"));
     
-    delay(3000); // Compensate for slow start Line6 M5
-    sendMidiProgramChange(programChangeLine6M5, presetLine6M5);
+    delay(3000); // Compensate for slow start Controlled Device
 
     xHeliOSSetup();
     
